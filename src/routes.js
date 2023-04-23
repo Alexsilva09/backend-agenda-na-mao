5de9dc57fe0ea3254e5d8b0b8eae0ea77f385646
Alexsilva09 <alexroquesilva2@gmail.com>
2023-04-23 16:29:14 -0300

    models criado com sucesso.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 import { Router } from "express"
+import {v4} from 'uuid'
 
+import User from './app/models/User'
 
 const routes = new Router()
 
 routes.get('/', async (request, response)=>{
     
+    const user = await User.create({
+        id: v4(),
+        name: 'Alex',
+        estabelecimento: 'alex cortes',
+        cod: '55',
+        tel: '94308921',
+        email: 'alexcds2021@live.com',
+        password_hash: '123',
 
-    return response.json()
-
+    })
+    return response.json(user)
 })
 
 export default routes
