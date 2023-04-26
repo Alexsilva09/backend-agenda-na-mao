301f8fb2a58d4e5cbb104b258847e657a149de08
Alexsilva09 <alexroquesilva2@gmail.com>
2023-04-25 21:14:40 -0300

    criação do login do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -24,12 +24,14 @@ class User extends Model {
             if(user.password){
                 user.password_hash = await bcrypt.hash(user.password, 10)
             }
-            return this
-
+            
         })
-
+        return this
     }
 
+    checkPassword(password){
+        return bcrypt.compare(password, this.password_hash)
+    }
 
 }
 
